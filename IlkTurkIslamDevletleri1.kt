c8fceae307977f73c17b5f487b6c2bf58a6ec46a
Bilal AKIN <bilalimakin@gmail.com>
2024-03-26 19:12:15 +0300

    Kaydet

---
+++ IlkTurkIslamDevletleri1.kt
@@ -0,0 +1,22 @@
+package com.example.e_tt.kpss.kpssTarih
+
+object IlkTurkIslamDevletleri1 {
+    val question = arrayOf(
+
+        "SORU 1?",
+        "SORU 2?",
+        "SORU 3?",
+    )
+
+    val choices = arrayOf(
+        arrayOf("A", "B"),
+        arrayOf("A", "B"),
+        arrayOf("A", "B")
+    )
+
+    val correctAnswers = arrayOf(
+        "B",
+        "A",
+        "B"
+    )
+}
